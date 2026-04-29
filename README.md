5bf667f526cbf900e9b1a5be2248c7fd68fa1f03
Rachel Pizane <rachel.maia@dbserver.com.br>
2026-04-28 21:36:25 -0300

    [PORTFOLIO-00] chore: iniciar projeto; configurar tema; padronizar formatação do codigo

---
+++ README.md
@@ -0,0 +1,1 @@
+# Portfólio DB
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <>
+            <h1 className="bg-primary">Eaque expedita ab et cupiditate</h1>
+            <p className="text-menu-text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio porro possimus iure
+                quod incidunt aperiam quia animi soluta earum quidem non eius libero laudantium
+                commodi, ratione, blanditiis aliquid, sint recusandae.
+            </p>
+        </>
+    );
+}
+
+export default App;
